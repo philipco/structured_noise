119f3c7b346ea7bc4fca2d6b8f00680cceb6b6f4
Constantin Philippenko <constantin.philippenko@polytechnique.edu>
2022-01-20 14:19:31 +0100

    Analyse noise of compression w.r.t. trace and eigen values

---
+++ CompressionModel.py
@@ -0,0 +1,218 @@
+"""
+Created by Constantin Philippenko, 29th December 2021.
+
+This python file provide facilities to quantize tensors.
+"""
+from abc import ABC, abstractmethod
+
+import numpy as np
+import tensorflow as tf
+import torch
+from scipy.stats import bernoulli
+from math import sqrt
+
+
+class CompressionModel(ABC):
+    """
+    The CompressionModel class declares the factory methods while subclasses provide the implementation of this methods.
+
+    This class defines the operators of compression.
+    """
+
+    def __init__(self, level: int, dim: int = None, norm: int = 2, constant: int = 1):
+        self.level = level
+        self.dim = dim
+        self.norm = norm
+        self.constant = constant
+        if dim is not None:
+            self.omega_c = self.__compute_omega_c__(dim=dim)
+        else:
+            self.omega_c = None
+
+    @abstractmethod
+    def __compress__(self, vector: np.ndarray, dim_to_use: int):
+        """Compresses a vector with the mechanism of the operator of compression."""
+        pass
+
+    def compress(self, vector: np.ndarray) -> np.ndarray:
+        """Prepare a vector for compression, and compresses it.
+
+        :param vector: The vector to be compressed.
+        :return: The compressed vector
+        """
+        if self.level == 0:
+            return vector
+        vector, dim = vector, vector.shape[0]
+
+        compressed_vector = self.__compress__(vector)
+        return compressed_vector
+
+    @abstractmethod
+    def __omega_c_formula__(self, dim_to_use: int):
+        """Proper implementation of the formula to compute omega_c.
+        This formula is unique for each operator of compression."""
+        pass
+
+    def __compute_omega_c__(self, vector: np.ndarray = None, dim: int = None):
+        """Compute the value of omega_c."""
+        # If s==0, it means that there is no compression.
+        # But for the need of experiments, we may need to compute the quantization constant associated with s=1.
+        if self.level == 0:
+            omega = 0
+        else:
+            omega = self.__omega_c_formula__(dim)
+        return omega
+
+    @abstractmethod
+    def get_name(self) -> str:
+        """Returns the name of the operator of compression."""
+        pass
+
+    def get_learning_rate(self, *args, **kwds):
+        """Compute the learning rate.
+
+        No argument if the operator already know the dimension.
+        Set as unique argument a vector ig the dimension is unknow (in the case of DL)."""
+        if self.level == 0:
+            return 0
+        if len(args) == 1:
+            return self.constant / (self.__compute_omega_c__(args[0]) + 1)
+        return self.constant / (self.omega_c + 1)
+
+
+class TopKSparsification(CompressionModel):
+
+    def __init__(self, level: int, dim: int = None, norm: int = 2, constant: int = 1):
+        super().__init__(level, dim, norm, constant)
+        if dim is not None:
+            assert 0 <= level < 1, "k is a probability."
+        self.biased = True
+
+    def __compress__(self, vector: np.ndarray):
+        indices = np.argpartition(vector, -self.level)[-self.level:]
+        compression = np.zeros_like(vector)
+        for i in indices:
+            compression[i.item()] = vector[i]
+        return compression
+
+    def __omega_c_formula__(self, dim_to_use: int):
+        proba = self.level
+        return 1 - proba
+
+    def get_name(self) -> str:
+        return "Topk"
+
+
+class RandomSparsification(CompressionModel):
+
+    def __init__(self, level: int, dim: int = None, biased=False, norm: int = 2, constant: int = 1):
+        """
+
+        :param level: number of dimension to select at compression step
+        :param dim: number of dimension in the dataset
+        :param biased: set to True to used to biased version of this operators
+        """
+        self.biased = biased
+        super().__init__(level, dim, norm, constant)
+        assert 0 <= level <= 1, "The level must be expressed in percent."
+
+    def __compress__(self, vector: np.ndarray):
+        proba = self.level
+        indices = bernoulli.rvs(proba, size=len(vector))
+        compression = np.zeros_like(vector)
+        for i in range(len(vector)):
+            if indices[i]:
+                compression[i] = vector[i] * [1 / proba, 1][self.biased]
+        return compression
+
+    def __omega_c_formula__(self, dim_to_use: int):
+        proba = self.level
+        # if self.biased:
+        #     return 1 - proba
+        return (1 - proba) / proba
+
+    def get_name(self) -> str:
+        if self.biased:
+            return "RdkBsd"
+        return "Rdk"
+
+
+class SQuantization(CompressionModel):
+
+# import numpy as np
+# from CompressionModel import SQuantization, SQuantization2
+# vec = np.array([-5,-15,11,9,8,0.01,0,-5,12,9,-7,3])
+# compressor = SQuantization(1, len(vec))
+# compressor2 = SQuantization2(1, len(vec))
+# compressor.compress(vec)
+
+    def __init__(self, level: int, dim: int = None, norm: int = 2, div_omega: int = 1, constant: int = 1):
+        self.biased = False
+        self.div_omega = div_omega
+        super().__init__(level, dim, norm, constant)
+
+    def __compress__(self, vector):
+
+        # vector = torch.from_numpy(vector)
+        # norm_x = torch.norm(vector, p=self.norm)
+        # if norm_x == 0:
+        #     return vector
+        # all_levels = torch.floor(self.level * torch.abs(vector) / norm_x + torch.rand_like(vector)) / self.level
+        # signed_level = torch.sign(vector) * all_levels
+        # qtzt = signed_level * norm_x
+        # return qtzt.numpy()
+
+        norm_x = np.linalg.norm(vector, ord=self.norm)
+        if norm_x == 0:
+            return vector
+        ratio = np.abs(vector) / norm_x
+        p = ratio * self.level - np.floor(ratio * self.level)
+
+        alea = np.random.binomial(1, p, len(vector))
+        all_levels = np.floor(self.level * ratio + alea) / self.level
+
+        signed_level = np.sign(vector) * all_levels
+        qtzt = signed_level * norm_x
+        return qtzt
+
+    def __omega_c_formula__(self, dim_to_use):
+        return min(dim_to_use / (self.level * self.level * self.div_omega),
+                   sqrt(dim_to_use) / (self.level * self.div_omega))
+
+    def get_name(self) -> str:
+        return "Qtzd"
+
+
+class RandomDithering(CompressionModel):
+
+    def __compress__(self, vector: np.ndarray) -> np.ndarray:
+
+        xnorm = np.linalg.norm(vector)
+        if xnorm == 0:
+            return vector
+
+        xx = np.random.uniform(0.0, 1.0, self.dim)
+        xsign = np.sign(vector)
+        x_int = np.floor(self.level * np.abs(vector) / xnorm + xx)
+        x_cpmpress = xnorm / self.level * xsign * x_int
+        return x_cpmpress
+
+    def __omega_c_formula__(self, dim_to_use):
+        return min(dim_to_use / (self.level * self.level),
+                   sqrt(dim_to_use) / (self.level))
+
+    def get_name(self) -> str:
+        return "Ditering"
+
+
+class SignSGD(CompressionModel):
+
+    def __compress__(self, vector):
+        # print(np.sign(vector))
+        return np.sign(vector)
+
+    def __omega_c_formula__(self, dim_to_use):
+        return 2
+
+    def get_name(self) -> str:
+        return "Sign"
---
+++ PlotEigenValues.py
@@ -0,0 +1,80 @@
+"""
+Created by Constantin Philippenko, 17th January 2022.
+"""
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+from CompressionModel import SQuantization, RandomSparsification
+from SyntheticDataset import SyntheticDataset
+
+SIZE_DATASET = 100000
+DIM = 50
+
+
+def compute_diag_matrices(dim: int):
+
+    dataset = SyntheticDataset()
+    dataset.generate_X(dim, size_dataset=SIZE_DATASET, power_cov=4, use_ortho_matrix=True)
+    X_quantized = dataset.X.copy()
+    X_sparsed = dataset.X.copy()
+
+    quantizator = SQuantization(1, dim=dim)
+
+    p = 1 / (quantizator.omega_c + 1)
+    print("Sparsification proba: ", p)
+    sparsificator = RandomSparsification(p, dim, biased=True)
+
+    ortho_matrix = dataset.ortho_matrix.copy()
+    # Matrix of probabilities for the sparsification operator.
+    if sparsificator.biased:
+        P = 1 / (p ** 2) * np.ones_like(ortho_matrix) + (1 / p - 1 / p ** 2) * np.identity(n=dim)
+    else:
+        P = p * np.identity(n=dim)
+
+    for i in range(SIZE_DATASET):
+        X_quantized[i] = quantizator.compress(dataset.X[i])
+        X_sparsed[i] = sparsificator.compress(dataset.X[i])
+
+    cov_matrix = dataset.X.T.dot(dataset.X) / SIZE_DATASET
+    cov_matrix_qtz = X_quantized.T.dot(X_quantized) / SIZE_DATASET
+    cov_matrix_sparse = P * (X_sparsed.T.dot(X_sparsed) / SIZE_DATASET)
+
+    diag = np.diag(dataset.ortho_matrix.T.dot(cov_matrix).dot(dataset.ortho_matrix))
+    diag_qtz = np.diag(dataset.ortho_matrix.T.dot(cov_matrix_qtz).dot(dataset.ortho_matrix))
+    diag_sparse = np.diag(dataset.ortho_matrix.T.dot(cov_matrix_sparse).dot(dataset.ortho_matrix))
+
+    return cov_matrix, cov_matrix_qtz, cov_matrix_sparse, diag, diag_qtz, diag_sparse
+
+
+if __name__ == '__main__':
+
+    cov_matrix, cov_matrix_qtz, cov_matrix_sparse, diag, diag_qtz, diag_sparse = compute_diag_matrices(dim=DIM)
+
+    plt.imshow(cov_matrix)
+    plt.colorbar()
+    plt.title("No compression", fontsize=15)
+    plt.show()
+
+    plt.imshow(cov_matrix_qtz)
+    plt.colorbar()
+    plt.title("Quantization", fontsize=15)
+    plt.show()
+
+    plt.imshow(cov_matrix_sparse)
+    plt.colorbar()
+    plt.title("Sparsification", fontsize=15)
+    plt.show()
+
+    fig, ax = plt.subplots(figsize=(8, 7))
+    plt.plot(np.log10(np.arange(1, DIM + 1)), np.log10(diag), label="No compression")
+    plt.plot(np.log10(np.arange(1, DIM + 1)), np.log10(diag_qtz), label="Quantization")
+    plt.plot(np.log10(np.arange(1, DIM + 1)), np.log10(diag_sparse), label="Sparsification")
+    ax.tick_params(axis='both', labelsize=15)
+    ax.legend(loc='best', fontsize=15)
+    ax.set_xlabel(r"$\log(i), \forall i \in \{1, ..., d\}$", fontsize=15)
+    ax.set_ylabel(r"$\log(Diag(\frac{X^T.X \Sigma}{n})_i)$", fontsize=15)
+    plt.legend(loc='best', fontsize=15)
+
+    plt.show()
+
