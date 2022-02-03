bc4698a85a76136bb1c6c4af651928742c33493d
Constantin Philippenko <constantin.philippenko@polytechnique.edu>
2022-02-03 18:20:24 +0100

    Sigma correction.

--- src/SGD.py
+++ src/SGD.py
@@ -5,10 +5,10 @@ Created by Constantin Philippenko, 10th January 2022.
 import copy
 from abc import abstractmethod, ABC
 
+from numba import jit
 import numpy as np
-import torch
-from sklearn.linear_model._logistic import _logistic_loss, _logistic_loss_and_grad
-from sklearn.utils.extmath import log_logistic, safe_sparse_dot
+from numpy.linalg import inv
+from scipy.linalg import sqrtm
 from tqdm import tqdm
 
 from scipy.special import expit
@@ -18,7 +18,8 @@ from src.PickleHandler import pickle_saver
 from src.SyntheticDataset import MAX_SIZE_DATASET
 from src.Utilities import print_mem_usage
 
-DISABLE = True
+DISABLE = False
+CORRECTION = False
 
 
 class SeriesOfSGD:
@@ -64,22 +65,38 @@ class SGD(ABC):
         np.random.seed(25)
         self.w0 = np.random.normal(0, 1, size = self.DIM)
         self.additive_stochastic_gradient = False
+        self.transition_matrix = inv(sqrtm(self.synthetic_dataset.upper_sigma))
+        self.inv_transition_matrix = sqrtm(self.synthetic_dataset.upper_sigma)
 
+    # @jit
     def compute_empirical_risk(self, w, data, labels):
-        if self.do_logistic_regression:
-            return -np.sum(np.log(expit(labels * (data @ w)))) / len(labels)
+        # if self.do_logistic_regression:
+        #     return -np.sum(np.log(expit(labels * (data @ w)))) / len(labels)
+        if CORRECTION:
+            data = data @ self.transition_matrix.T
         return 0.5 * np.linalg.norm(data @ w - labels) ** 2 / len(labels)
 
+    # @jit
     def compute_true_risk(self, w, data, labels):
-        if self.do_logistic_regression:
-            return -np.sum(log_logistic(labels * (data @ w))) / len(labels)
+        if data is None:
+            return 0
+        # data = data @ self.transition_matrix.T
+        # if self.do_logistic_regression:
+        #     return -np.sum(log_logistic(labels * (data @ w))) / len(labels)
+        # w = self.inv_transition_matrix @ w
+        if CORRECTION:
+            w_star = self.inv_transition_matrix @ self.w_star
+            return 0.5 * (w - w_star).T @ (w - w_star)
         return 0.5 * (w - self.w_star).T @ self.synthetic_dataset.upper_sigma @ (w - self.w_star)
 
+    # @jit
     def compute_stochastic_gradient(self, w, data, labels, index):
         x, y = data[index], labels[index]
-        if self.do_logistic_regression:
-            s = expit(y * x @ w)
-            return x * ((s - 1) * y)
+        # if self.do_logistic_regression:
+        #     s = expit(y * x @ w)
+        #     return x * ((s - 1) * y)
+        if CORRECTION:
+            x = self.transition_matrix @ x
         return np.array((x @ w - y)).dot(x)
 
     def compute_additive_stochastic_gradient(self, w, data, labels, index):
@@ -107,9 +124,9 @@ class SGD(ABC):
                 gamma = self.synthetic_dataset.gamma
                 it += 1
                 if self.additive_stochastic_gradient:
-                    grad = self.compute_additive_stochastic_gradient(current_w, self.X, self.Y, idx)
+                    grad = self.compute_additive_stochastic_gradient(current_w, self.X, self.Y, idx % MAX_SIZE_DATASET)
                 else:
-                    grad = self.compute_stochastic_gradient(current_w, self.X, self.Y, idx)
+                    grad = self.compute_stochastic_gradient(current_w, self.X, self.Y, idx % MAX_SIZE_DATASET)
                 g = self.gradient_processing(grad)
                 matrix_grad[idx] = g
                 current_w = self.sgd_update(current_w, g, gamma)
