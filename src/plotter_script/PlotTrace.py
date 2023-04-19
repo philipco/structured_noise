567da673df9e3744c6180ef954ccf38221d7036b
Constantin Philippenko <constantin.philippenko@polytechnique.edu>
2023-04-20 00:41:47 +0200

    Trace

--- src/plotter_script/PlotTrace.py
+++ src/plotter_script/PlotTrace.py
@@ -28,7 +28,7 @@ R_SIGMA=0
 NB_CLIENTS = 1
 
 START_DIM = 2
-END_DIM = 20
+END_DIM = 100
 
 FONTSIZE = 17
 LINESIZE = 3
@@ -84,45 +84,46 @@ if __name__ == '__main__':
 
     range_trace = np.arange(START_DIM, END_DIM)
 
-    for use_ortho, power_cov in zip([True, False], [1,2,3,4,-1,"gap", "isotropic"]):
-        trace_by_operators = [[] for i   in range(len(labels))]
-        theoretical_trace_by_operators = [[] for i in range(len(labels))]
+    for use_ortho in [True, False]:
+        for power_cov in [1,2,3,4,-1,"gap", "isotropic"]:
+            trace_by_operators = [[] for i   in range(len(labels))]
+            theoretical_trace_by_operators = [[] for i in range(len(labels))]
 
-        for dim in range_trace:
-            clients = [Client(i, dim, SIZE_DATASET // NB_CLIENTS, power_cov, NB_CLIENTS, use_ortho, HETEROGENEITY) for i in
-                       range(NB_CLIENTS)]
-            dataset = SyntheticDataset()
-            all_trace, dataset = compute_trace(dataset, dim, use_ortho, power_cov)
+            for dim in range_trace:
+                clients = [Client(i, dim, SIZE_DATASET // NB_CLIENTS, power_cov, NB_CLIENTS, use_ortho, HETEROGENEITY) for i in
+                           range(NB_CLIENTS)]
+                dataset = SyntheticDataset()
+                all_trace, dataset = compute_trace(dataset, dim, use_ortho, power_cov)
+                for i in range(len(labels)):
+                    trace_by_operators[i].append(all_trace[i])
+                all_theoretical_trace = [compute_theoretical_trace(dataset, "No compression"),
+                                         compute_theoretical_trace(dataset, "Qtzd"),
+                                         compute_theoretical_trace(dataset, "Sparsification"),
+                                         compute_theoretical_trace(dataset, "Sketching"),
+                                         compute_theoretical_trace(dataset, "Rand1"),
+                                         compute_theoretical_trace(dataset, "PartialParticipation")]
+                for i in range(len(labels)):
+                    theoretical_trace_by_operators[i].append(all_theoretical_trace[i])
+
+
+            fig, axes = plt.subplots(figsize=(6, 6))
             for i in range(len(labels)):
-                trace_by_operators[i].append(all_trace[i])
-            all_theoretical_trace = [compute_theoretical_trace(dataset, "No compression"),
-                                     compute_theoretical_trace(dataset, "Qtzd"),
-                                     compute_theoretical_trace(dataset, "Sparsification"),
-                                     compute_theoretical_trace(dataset, "Sketching"),
-                                     compute_theoretical_trace(dataset, "Rand1"),
-                                     compute_theoretical_trace(dataset, "PartialParticipation")]
-            for i in range(len(labels)):
-                theoretical_trace_by_operators[i].append(all_theoretical_trace[i])
+                axes.plot(np.log10(range_trace), np.log10(trace_by_operators[i]), label=labels[i], lw=LINESIZE, color=COLORS[i])
+                axes.plot(np.log10(range_trace), np.log10(theoretical_trace_by_operators[i]), label='_nolegend_', lw=LINESIZE,
+                          color=COLORS[i], linestyle="--")
 
+            axes.tick_params(axis='both', labelsize=FONTSIZE)
+            axes.legend(loc='best', fontsize=FONTSIZE)
+            axes.set_xlabel(r"$\log(i), \forall i \in \{1, ..., d\}$", fontsize=FONTSIZE)
+            # axes.set_title('Empirical (plain) vs theoretical trace (dashed)')
+            axes.set_ylabel(r"$\log(\mathrm{Tr}(\mathfrak{C}^{\mathrm{ania}} H^{-1}))$", fontsize=FONTSIZE)
 
-        fig, axes = plt.subplots(figsize=(6, 6))
-        for i in range(len(labels)):
-            axes.plot(np.log10(range_trace), np.log10(trace_by_operators[i]), label=labels[i], lw=LINESIZE, color=COLORS[i])
-            axes.plot(np.log10(range_trace), np.log10(theoretical_trace_by_operators[i]), label='_nolegend_', lw=LINESIZE,
-                      color=COLORS[i], linestyle="--")
+            print("Script completed.")
+            folder = "pictures/trace/"
+            create_folder_if_not_existing(folder)
 
-        axes.tick_params(axis='both', labelsize=FONTSIZE)
-        axes.legend(loc='best', fontsize=FONTSIZE)
-        axes.set_xlabel(r"$\log(i), \forall i \in \{1, ..., d\}$", fontsize=FONTSIZE)
-        # axes.set_title('Empirical (plain) vs theoretical trace (dashed)')
-        axes.set_ylabel(r"$\log(\mathrm{Tr}(\mathfrak{C}^{\mathrm{ania}} H^{-1}))$", fontsize=FONTSIZE)
-
-        print("Script completed.")
-        folder = "pictures/trace/"
-        create_folder_if_not_existing(folder)
-
-        hash = dataset.string_for_hash(nb_runs=1)
-        plt.savefig("{0}/C{1}-{2}.pdf".format(folder, NB_CLIENTS, hash), bbox_inches='tight', dpi=600)
+            hash = dataset.string_for_hash(nb_runs=1)
+            plt.savefig("{0}/C{1}-{2}.pdf".format(folder, NB_CLIENTS, hash), bbox_inches='tight', dpi=600)
 
     # plt.show()
 
